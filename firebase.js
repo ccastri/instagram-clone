cbb6edbdd7fa8af3df53c5feb704423c39337870
ccastri <camilo.castrillon@uao.edu.co>
2022-10-29 13:43:25 -0500

    updated firestore

--- firebase.js
+++ firebase.js
@@ -2,29 +2,16 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
-// import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDadqGnSedRZxGaukFOgIl_hZearXRcGgA",
-    authDomain: "instagram-clone-4276d.firebaseapp.com",
-    projectId: "instagram-clone-4276d",
-    storageBucket: "instagram-clone-4276d.appspot.com",
-    messagingSenderId: "1045332612298",
-    appId: "1:1045332612298:web:eea41955e7b3d1a0e09033",
-    measurementId: "G-LKSTEHQETP"
+    apiKey: "AIzaSyCm9ah_3jYtP4Zbi-oWrtsRMtNmx2cHWc0",
+    authDomain: "insta-clone-db84f.firebaseapp.com",
+    projectId: "insta-clone-db84f",
+    storageBucket: "insta-clone-db84f.appspot.com",
+    messagingSenderId: "783513979725",
+    appId: "1:783513979725:web:2401d4cf694b2a08234504"
 };
-
-// Initialize Firebase
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,11 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import Feed from '../components/Feed'
 import Header from '../components/Header'
 import Modal from '../components/Modal'
-import Feed from '../components/Feed'
-import { useSession, signOut, signIn } from "next-auth/react"
 
 const Home: NextPage = () => {
-  const {data: session} =useSession();
+
   return (
     <div className="">
       <Head>
